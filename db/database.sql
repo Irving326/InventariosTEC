936864d99cfbc7dfe1ceb0852379ebe20f77357c
Irving326 <irvingloza326@gmail.com>
2026-06-30 10:55:36 -0600

    Actualiza database.sql en carpeta db con solo la estructura

--- db/database.sql
+++ db/database.sql
@@ -1,35 +1,388 @@
-CREATE TABLE productos (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    stock INT DEFAULT 0,
-    precio DECIMAL(10,2) NOT NULL
+--
+-- PostgreSQL database dump
+--
+
+\restrict rnhz5d7Z1gugvJObaGxgamJ036GtdwKcHbY4evHh2UXvmTqf8UDETR8gewQOejN
+
+-- Dumped from database version 18.4 (Ubuntu 18.4-0ubuntu0.26.04.1)
+-- Dumped by pg_dump version 18.4 (Ubuntu 18.4-0ubuntu0.26.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: detalle_ventas; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.detalle_ventas (
+    id integer NOT NULL,
+    venta_id integer,
+    producto_id integer,
+    cantidad integer NOT NULL,
+    precio_unitario numeric(10,2) NOT NULL,
+    subtotal numeric(10,2) NOT NULL
 );
 
-CREATE TABLE proveedores (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
+
+ALTER TABLE public.detalle_ventas OWNER TO postgres;
+
+--
+-- Name: detalle_ventas_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.detalle_ventas_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.detalle_ventas_id_seq OWNER TO postgres;
+
+--
+-- Name: detalle_ventas_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.detalle_ventas_id_seq OWNED BY public.detalle_ventas.id;
+
+
+--
+-- Name: historial_stock; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.historial_stock (
+    id integer NOT NULL,
+    producto_id integer,
+    proveedor_id integer,
+    cantidad integer NOT NULL,
+    fecha_movimiento timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE historial_stock (
-    id SERIAL PRIMARY KEY,
-    producto_id INT REFERENCES productos(id),
-    proveedor_id INT REFERENCES proveedores(id) ON DELETE SET NULL,
-    cantidad INT NOT NULL,
-    fecha_movimiento TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+ALTER TABLE public.historial_stock OWNER TO postgres;
+
+--
+-- Name: historial_stock_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.historial_stock_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.historial_stock_id_seq OWNER TO postgres;
+
+--
+-- Name: historial_stock_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.historial_stock_id_seq OWNED BY public.historial_stock.id;
+
+
+--
+-- Name: productos; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.productos (
+    id integer NOT NULL,
+    nombre text,
+    precio numeric,
+    stock numeric,
+    descripcion text,
+    categoria_id text,
+    imagen text,
+    codigo text,
+    proveedor_id integer
 );
 
-CREATE TABLE ventas (
-    id SERIAL PRIMARY KEY,
-    cliente_nombre VARCHAR(100) DEFAULT 'Público General',
-    total DECIMAL(10,2) NOT NULL,
-    fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+ALTER TABLE public.productos OWNER TO postgres;
+
+--
+-- Name: productos_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.productos_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.productos_id_seq OWNER TO postgres;
+
+--
+-- Name: productos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.productos_id_seq OWNED BY public.productos.id;
+
+
+--
+-- Name: proveedores; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.proveedores (
+    id integer NOT NULL,
+    nombre text,
+    direccion text,
+    telefono text,
+    correo text,
+    descripcion text
 );
 
-CREATE TABLE detalle_ventas (
-    id SERIAL PRIMARY KEY,
-    venta_id INT REFERENCES ventas(id) ON DELETE CASCADE,
-    producto_id INT REFERENCES productos(id),
-    cantidad INT NOT NULL,
-    precio_unitario DECIMAL(10,2) NOT NULL,
-    subtotal DECIMAL(10,2) NOT NULL
+
+ALTER TABLE public.proveedores OWNER TO postgres;
+
+--
+-- Name: proveedores_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.proveedores_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.proveedores_id_seq OWNER TO postgres;
+
+--
+-- Name: proveedores_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.proveedores_id_seq OWNED BY public.proveedores.id;
+
+
+--
+-- Name: usuarios; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.usuarios (
+    id integer NOT NULL,
+    username text,
+    password text,
+    email text,
+    rol_id numeric
 );
+
+
+ALTER TABLE public.usuarios OWNER TO postgres;
+
+--
+-- Name: usuarios_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.usuarios_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.usuarios_id_seq OWNER TO postgres;
+
+--
+-- Name: usuarios_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.usuarios_id_seq OWNED BY public.usuarios.id;
+
+
+--
+-- Name: ventas; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ventas (
+    id integer NOT NULL,
+    cliente_nombre character varying(100) DEFAULT 'Público General'::character varying,
+    total numeric(10,2) NOT NULL,
+    fecha_venta timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.ventas OWNER TO postgres;
+
+--
+-- Name: ventas_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.ventas_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.ventas_id_seq OWNER TO postgres;
+
+--
+-- Name: ventas_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.ventas_id_seq OWNED BY public.ventas.id;
+
+
+--
+-- Name: detalle_ventas id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.detalle_ventas ALTER COLUMN id SET DEFAULT nextval('public.detalle_ventas_id_seq'::regclass);
+
+
+--
+-- Name: historial_stock id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.historial_stock ALTER COLUMN id SET DEFAULT nextval('public.historial_stock_id_seq'::regclass);
+
+
+--
+-- Name: productos id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.productos ALTER COLUMN id SET DEFAULT nextval('public.productos_id_seq'::regclass);
+
+
+--
+-- Name: proveedores id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.proveedores ALTER COLUMN id SET DEFAULT nextval('public.proveedores_id_seq'::regclass);
+
+
+--
+-- Name: usuarios id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.usuarios ALTER COLUMN id SET DEFAULT nextval('public.usuarios_id_seq'::regclass);
+
+
+--
+-- Name: ventas id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ventas ALTER COLUMN id SET DEFAULT nextval('public.ventas_id_seq'::regclass);
+
+
+--
+-- Name: detalle_ventas detalle_ventas_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.detalle_ventas
+    ADD CONSTRAINT detalle_ventas_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: historial_stock historial_stock_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.historial_stock
+    ADD CONSTRAINT historial_stock_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: productos productos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.productos
+    ADD CONSTRAINT productos_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: proveedores proveedores_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.proveedores
+    ADD CONSTRAINT proveedores_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: usuarios usuarios_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.usuarios
+    ADD CONSTRAINT usuarios_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ventas ventas_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ventas
+    ADD CONSTRAINT ventas_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: detalle_ventas detalle_ventas_producto_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.detalle_ventas
+    ADD CONSTRAINT detalle_ventas_producto_id_fkey FOREIGN KEY (producto_id) REFERENCES public.productos(id);
+
+
+--
+-- Name: detalle_ventas detalle_ventas_venta_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.detalle_ventas
+    ADD CONSTRAINT detalle_ventas_venta_id_fkey FOREIGN KEY (venta_id) REFERENCES public.ventas(id) ON DELETE CASCADE;
+
+
+--
+-- Name: productos fk_productos_proveedores; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.productos
+    ADD CONSTRAINT fk_productos_proveedores FOREIGN KEY (proveedor_id) REFERENCES public.proveedores(id) ON DELETE SET NULL;
+
+
+--
+-- Name: historial_stock historial_stock_producto_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.historial_stock
+    ADD CONSTRAINT historial_stock_producto_id_fkey FOREIGN KEY (producto_id) REFERENCES public.productos(id) ON DELETE CASCADE;
+
+
+--
+-- Name: historial_stock historial_stock_proveedor_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.historial_stock
+    ADD CONSTRAINT historial_stock_proveedor_id_fkey FOREIGN KEY (proveedor_id) REFERENCES public.proveedores(id) ON DELETE SET NULL;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict rnhz5d7Z1gugvJObaGxgamJ036GtdwKcHbY4evHh2UXvmTqf8UDETR8gewQOejN
+
